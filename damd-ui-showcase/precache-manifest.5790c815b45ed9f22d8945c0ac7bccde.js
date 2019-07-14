f139d3b38f5bc74ce46971aad61bb20a63f6f5f8
Yuri Harrison <yuriharrisonlm@gmail.com>
2019-07-14 17:14:26 -0300

    DAMD-UI Showcase

---
+++ damd-ui-showcase/precache-manifest.5790c815b45ed9f22d8945c0ac7bccde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24694a98572d183a5e10dc4565ba14c0",
+    "url": "/damd-ui-showcase/index.html"
+  },
+  {
+    "revision": "86301d1c2dc1565e6fe3",
+    "url": "/damd-ui-showcase/static/css/main.f824fbc8.chunk.css"
+  },
+  {
+    "revision": "c70d5a710404e36c3ff9",
+    "url": "/damd-ui-showcase/static/js/2.21b2a483.chunk.js"
+  },
+  {
+    "revision": "86301d1c2dc1565e6fe3",
+    "url": "/damd-ui-showcase/static/js/main.e9c7c1d5.chunk.js"
+  },
+  {
+    "revision": "1d34433d78956b68ef9a",
+    "url": "/damd-ui-showcase/static/js/runtime~main.5b84f2c3.js"
+  }
+]);
